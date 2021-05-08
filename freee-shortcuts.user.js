f501022ab3be9261246895d3337521cfbd67d75f
Masaki Kobayashi <contact@mkobayashi.me>
2021-05-08 22:57:07 +0900

    Add freee-shortcuts.user.js

---
+++ freee-shortcuts.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         freee shortcut keys
+// @namespace    mkobayashime
+// @version      1.0.0
+// @description  freee にショートカットキーを追加します
+// @author       mkobayashime
+// @homepage     https://github.com/mkobayashime/userscripts
+// @updateURL    https://github.com/mkobayashime/userscripts/raw/main/freee-shortcuts.user.js
+// @include      https://secure.freee.co.jp/*
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  window.onkeyup = (e) => {
+    if (e.key === "Escape") {
+      const xpath = "//button[contains(text(),'閉じる')]";
+      const matchingElement = document.evaluate(
+        xpath,
+        document,
+        null,
+        XPathResult.FIRST_ORDERED_NODE_TYPE,
+        null
+      ).singleNodeValue;
+
+      if (matchingElement) matchingElement.click();
+    }
+  };
+})();
