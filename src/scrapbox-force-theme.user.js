32f7a0a9597ab401c5d752eee6d1c9d21728218f
Masaki Kobayashi <contact@mkobayashi.me>
2021-05-16 22:09:10 +0900

    Add scrapbox-force-theme.user.js

---
+++ src/scrapbox-force-theme.user.js
@@ -0,0 +1,60 @@
+// ==UserScript==
+// @name         Scrapbox Force Theme
+// @namespace    mkobayashime
+// @version      1.0.0
+// @description  Scrapbox でプロジェクトに関わらず特定のテーマを使用します
+// @author       mkobayashime
+// @homepage     https://github.com/mkobayashime/userscripts
+// @updateURL    https://github.com/mkobayashime/userscripts/raw/main/src/scrapbox-force-theme.user.js
+// @include      https://scrapbox.io/*
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
+
+;(function () {
+  "use strict"
+
+  /**
+   * 適用するテーマ. html の data-project-theme に設定されるもの.
+   * @type string
+   */
+  const themeId = "default-dark"
+
+  /**
+   * 有効化するプロジェクトの id またはマッチする RegExp. 空配列の場合全プロジェクトで有効.
+   * @type Array<string | RegExp>
+   */
+  const enabledProjectIds = []
+
+  const isProjectEnabled = (projectId) => {
+    if (enabledProjectIds.length === 0) return true
+    return enabledProjectIds.some((pattern) => {
+      if (typeof pattern === "string") {
+        return pattern === projectId
+      }
+      return pattern.test(projectId)
+    })
+  }
+
+  const pageObserver = new MutationObserver(() => {
+    if (document.documentElement.dataset.projectTheme !== themeId) {
+      document.documentElement.dataset.projectTheme = themeId
+    }
+  })
+
+  const settingPagesPattern = new RegExp(
+    "https://scrapbox.io/(projects/[^/]+/)?settings/"
+  )
+
+  const url = window.location.href
+
+  if (!settingPagesPattern.test(url)) {
+    const projectId = window.location.href.match(
+      RegExp("^https://scrapbox.io/(?<projectId>.*)/.*$")
+    ).groups.projectId
+
+    if (isProjectEnabled(projectId)) {
+      pageObserver.observe(document.documentElement, { attributes: true })
+    }
+  }
+})()
