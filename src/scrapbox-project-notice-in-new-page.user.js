d17729a4c80bf9304430d2ee061f574ab1c145fb
Masaki Kobayashi <contact@mkobayashi.me>
2021-08-13 00:47:09 +0900

    Add scrapbox-project-notice-in-new-page.user.js

---
+++ src/scrapbox-project-notice-in-new-page.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Scrapbox - Project name notice in new page
+// @namespace    mkobayashime
+// @version      1.0.0
+// @author       mkobayashime
+// @match        https://scrapbox.io/*
+// @icon         https://www.google.com/s2/favicons?domain=scrapbox.io
+// @homepage     https://github.com/mkobayashime/userscripts
+// @updateURL    https://github.com/mkobayashime/userscripts/raw/main/src/scrapbox-project-notice-in-new-page.user.js
+// @grant        none
+// ==/UserScript==
+
+;(function () {
+  "use strict"
+
+  new MutationObserver(() => {
+    if (
+      window.scrapbox.Page.lines?.length === 1 &&
+      window.scrapbox.Page.lines[0]?.text === ""
+    ) {
+      window.alert(
+        `You are about to create a new page in "${
+          window.scrapbox.Project?.name ?? "unknown project"
+        }". Are you sure?`
+      )
+    }
+  }).observe(document.querySelector("title"), { childList: true })
+})()
