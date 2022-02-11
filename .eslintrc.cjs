87c0b17fe40a5526d2a1936f6cce6af851673966
Masaki Kobayashi <contact@mkobayashi.me>
2022-02-11 17:30:46 +0900

    Make use of `eslint-plugin-userscripts`

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["eslint:recommended", "prettier"],
+  extends: ["eslint:recommended", "plugin:userscripts/recommended", "prettier"],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
