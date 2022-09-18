b4403a851c1f712f5262204c8f33d42199ead011
Masaki Kobayashi <contact@mkobayashi.me>
2022-09-18 20:35:17 +0900

    Update scaffold script

--- bin/scaffold-script.sh
+++ bin/scaffold-script.sh
@@ -7,21 +7,8 @@ echo 'Description of the userscript:'
 read -r description
 
 cat << TEMPLATE > "src/$script_name.user.ts"
-// ==UserScript==
-// @name         $script_name // FIXME:
-// @namespace    mkobayashime
-// @version      1.0.0
-// @description  $description
-// @author       mkobayashime
-// @homepage     https://github.com/mkobayashime/userscripts
-// @updateURL    https://github.com/mkobayashime/userscripts/raw/main/dist/$script_name.user.js
-// @match        https://exapmle.com/* // FIXME:
-// @icon         https://www.google.com/s2/favicons?domain=exapmle.com // FIXME:
-// @run-at       document-end
-// @grant        none
-// ==/UserScript==
-
-const config = {};
+const config = {
+};
 
 (({}: typeof config) => {
   //
@@ -29,3 +16,17 @@ const config = {};
 
 export {};
 TEMPLATE
+
+existing_meta=$(head -n -1 src/userscripts/meta/index.ts)
+
+cat << META > "src/userscripts/meta/index.ts"
+$existing_meta
+  "$script_name": {
+    name: "",
+    description: "$description",
+    version: "0.1.0",
+    match: "https://example.com/*",
+    icon: "https://www.google.com/s2/favicons?domain=example.com"
+  },
+}
+META
