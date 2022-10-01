c97b53dc165d94f506499f535b6636b246569cca
Masaki Kobayashi <contact@mkobayashi.me>
2022-10-02 00:41:28 +0900

    Refactor test for meta

--- src/userscripts/meta/index.test.ts
+++ src/userscripts/meta/index.test.ts
@@ -5,20 +5,14 @@ import glob from "glob";
 import { meta } from "./index.js";
 
 const tsScripts = glob.sync("./src/userscripts/*.user.ts");
+const scriptNames = tsScripts.map((file) => path.basename(file, ".user.ts"));
 
-test("All scripts have their own meta", (t) => {
-  const scriptNames = tsScripts.map((file) => path.basename(file, ".user.ts"));
-
-  t.true(
-    scriptNames.every((name) => {
-      const scriptMeta = meta[name];
-      if (!scriptMeta) {
-        console.error(`Error: Meta not found for ${name}`);
-      }
-      return Boolean(scriptMeta);
-    })
-  );
-});
+for (const name of scriptNames) {
+  test(`${name} has meta`, (t) => {
+    const scriptMeta = meta[name];
+    return t.true(Boolean(scriptMeta));
+  });
+}
 
 test("The number of scripts equals to the number of meta", (t) => {
   t.is(tsScripts.length, Object.keys(meta).length);
