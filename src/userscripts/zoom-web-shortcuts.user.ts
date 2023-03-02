1b470faa5e1ebfa2a77c5fe58227470164708295
Masaki Kobayashi <contact@mkobayashi.me>
2023-03-02 17:13:23 +0900

    Add `zoom-web-shortcuts`

---
+++ src/userscripts/zoom-web-shortcuts.user.ts
@@ -0,0 +1,27 @@
+(() => {
+  window.addEventListener("keydown", (e) => {
+    if (e.ctrlKey && e.key === "d") {
+      e.preventDefault();
+
+      const microphoneButton = document.querySelector(
+        '.footer-button__wrapper button[aria-label*="microphone"]'
+      );
+      if (microphoneButton && microphoneButton instanceof HTMLElement) {
+        microphoneButton.click();
+      }
+    }
+
+    if (e.ctrlKey && e.key === "e") {
+      e.preventDefault();
+
+      const videoButton = document.querySelector(
+        '.footer-button__wrapper button[aria-label*="video"]'
+      );
+      if (videoButton && videoButton instanceof HTMLElement) {
+        videoButton.click();
+      }
+    }
+  });
+})();
+
+export {};
