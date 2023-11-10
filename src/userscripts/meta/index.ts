1674598d02d1954bb4243f490058f90242387f4b
Masaki Kobayashi <contact@mkobayashi.me>
2023-11-11 00:27:59 +0900

    Ensure it works in the latest application

--- src/userscripts/meta/index.ts
+++ src/userscripts/meta/index.ts
@@ -77,7 +77,7 @@ export const meta: { [name: string]: UserScriptMeta | undefined } = {
     match: "https://www.instagram.com/*",
     name: "Instagram - Shortcut keys",
     runAt: "document-end",
-    version: "2.2.1",
+    version: "2.2.2",
   },
   "lifull-homes-shortcuts": {
     description: "Next/Prev image with arrow/h/l keys",
