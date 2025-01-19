2124bc58eef90f10690be925bdc8719da28821c3
Masaki Kobayashi <contact@mkobayashi.me>
2025-01-19 19:10:20 +0900

    Add `github-auto-sso`

---
+++ src/userscripts/github-auto-sso.user.ts
@@ -0,0 +1,21 @@
+(() => {
+  if (window.location.pathname.endsWith("/sso")) {
+    const ssoFormSubmitButton = document.querySelector(
+      ".business-sso-panel form button[type='submit']",
+    );
+    if (ssoFormSubmitButton instanceof HTMLElement) {
+      ssoFormSubmitButton.click();
+    }
+
+    return;
+  }
+
+  const ssoAnchor = document.querySelector(
+    "section[aria-labelledby='single-sign-on'] a",
+  );
+  if (!(ssoAnchor instanceof HTMLElement)) return;
+
+  ssoAnchor.click();
+})();
+
+export {};
