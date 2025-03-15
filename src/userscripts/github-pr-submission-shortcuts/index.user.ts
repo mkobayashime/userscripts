423e169f586597b5848f0324eefeb06e319efdb6
Masaki Kobayashi <contact@mkobayashi.me>
2025-03-15 16:31:08 +0900

    Ensure `github-pr-submission-shortcuts` works in the latest UI of GitHub

--- src/userscripts/github-pr-submission-shortcuts/index.user.ts
+++ src/userscripts/github-pr-submission-shortcuts/index.user.ts
@@ -1,31 +1,60 @@
 import { defineUserScript } from "bundlemonkey";
+import { awaitWithInterval } from "../utils/awaitWithInterval";
 import { isTyping } from "../utils/isTyping";
 
 export default defineUserScript({
   name: "GitHub - PR submission shortcuts",
-  version: "1.2.2",
+  version: "1.3.0",
   description: "Ctrl+Enter to merge/automerge PR",
   match: ["https://github.com/*"],
   icon: "https://www.google.com/s2/favicons?domain=github.com",
   main: () => {
-    window.addEventListener("keydown", (e) => {
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    window.addEventListener("keydown", async (e) => {
       if (!/\/\S+\/\S+\/pull\//.test(window.location.pathname)) return;
 
       if (e.ctrlKey && e.key === "Enter" && !isTyping()) {
-        const confirmButton = document.querySelector<HTMLElement>(
-          "button.js-merge-commit-button[type='submit']",
-        );
-        if (confirmButton) {
-          confirmButton.click();
+        const mergeButton = await awaitWithInterval(() => {
+          const button = document.evaluate(
+            "//button[*//*[text() = 'Merge pull request' or text() = 'Enable auto-merge']]",
+            document,
+            null,
+            XPathResult.FIRST_ORDERED_NODE_TYPE,
+            null,
+          ).singleNodeValue;
+
+          if (button) {
+            return {
+              data: button,
+            };
+          }
+        });
+        if (!(mergeButton instanceof HTMLElement)) {
           return;
         }
 
-        const automergeConfirmButton = document.querySelector<HTMLElement>(
-          "button.js-confirm-auto-merge-button[type='submit']",
-        );
-        if (automergeConfirmButton) {
-          automergeConfirmButton.click();
+        mergeButton.click();
+
+        const confirmButton = await awaitWithInterval(() => {
+          const button = document.evaluate(
+            "//button[*//*[text() = 'Confirm merge' or text() = 'Confirm auto-merge']]",
+            document,
+            null,
+            XPathResult.FIRST_ORDERED_NODE_TYPE,
+            null,
+          ).singleNodeValue;
+
+          if (button) {
+            return {
+              data: button,
+            };
+          }
+        });
+        if (!(confirmButton instanceof HTMLElement)) {
+          return;
         }
+
+        confirmButton.click();
       }
     });
   },
